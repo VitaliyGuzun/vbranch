eef89840a22d17e0d812ee2095c86c106df88ed4
Vitaly Guzun <vitaly.guzun@gmail.com>
2025-05-05 20:35:46 +0300

    Get remote branches

--- branches.go
+++ branches.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"os/exec"
@@ -8,7 +10,7 @@ import (
 )
 
 // Проверка, что мы в git-репозитории
-func checkGitRepo() error {
+func isGitRepo() error {
 	cmd := exec.Command("git", "rev-parse", "--is-inside-work-tree")
 	output, err := cmd.Output()
 
@@ -19,12 +21,50 @@ func checkGitRepo() error {
 	return nil
 }
 
+// // Получение всех удалённых веток
+func getRemoteBranches() ([]string, error) {
+	cmd := exec.Command("git", "branch", "-r")
+	output, err := cmd.Output()
+
+	if err != nil {
+		return nil, err
+	}
+
+	branches := []string{}
+	scanner := bufio.NewScanner(bytes.NewReader(output))
+
+	for scanner.Scan() {
+		branch := strings.TrimSpace(scanner.Text())
+
+		if strings.Contains(branch, "->") {
+			// Пропускаем алиасы типа origin/HEAD -> origin/main
+			continue
+		}
+
+		branches = append(branches, branch)
+	}
+
+	return branches, nil
+}
+
 func main() {
-	error := checkGitRepo()
+	error := isGitRepo()
 
 	if error != nil {
 		log.Fatalf("ERROR: %v", error)
 	}
+
+	branches, err := getRemoteBranches()
+
+	if err != nil {
+		log.Fatalf("Ошибка получения веток: %v", err)
+	}
+
+	if len(branches) == 0 {
+		log.Fatalf("Нет веток на origin")
+	}
+
+	fmt.Println(branches)
 
 	log.Println("Все ок")
 }
